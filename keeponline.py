0d06db1a3cf4d214f7d80522e69d98f55979fd37
ZKeeer <zangker@foxmail.com>
2017-07-05 00:29:34 +0800

    2017年7月5日00:29:17 修复bug

--- keeponline.py
+++ keeponline.py
@@ -7,10 +7,10 @@ class KeepOnline:
     curr_hour = 0
 
     def __init__(self):
-        self.curr_hour = time.localtime().tm_mday
+        self.curr_hour = time.localtime().tm_hour
 
     def IsActived(self):
-        if self.curr_hour == time.localtime().tm_mday:
+        if self.curr_hour == time.localtime().tm_hour:
             return True
         else:
             return False
